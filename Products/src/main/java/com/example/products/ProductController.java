355129f72b26164e0748ca2e28e7d5710ffa16f7
Rasa Valanciunaite <fiammara@gmail.com>
2023-09-22 21:45:19 +0300

    Controller class edit

--- Products/src/main/java/com/example/products/ProductController.java
+++ Products/src/main/java/com/example/products/ProductController.java
@@ -1,42 +1,44 @@
 package com.example.products;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
 public class ProductController {
-    @Autowired
-    ProductService productService;
-
-    @GetMapping("/")
-    public String displayTodoList(@RequestParam(required = false) String message,
-                                  @RequestParam(required = false) String error,
-                                  Model model) {
-        model.addAttribute("message", message);
-        model.addAttribute("error", error);
-        model.addAttribute("productList", productService.findAllProducts());
-        return "productList";
+    private ProductService productService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
+    @GetMapping("/product-list")
+    public String displayUserList(@RequestParam(required = false) String message,
+                                  @RequestParam(required = false) String error,
+                                  Model model){
+        model.addAttribute("message", message);
+        model.addAttribute("error", error);
+        model.addAttribute("productList", this.productService.getAllProducts());
+        return "productList";
+    }
     @GetMapping("/add-product")
-    public String displayAddProductPage() {
+    public String displayAddProductPage(){
         return "addProduct";
     }
 
     @PostMapping("/add-product")
-    public String createTodo(Product product) {
+    public String createProduct(Product product) {
         try {
-            System.out.println("Created product >>>> " + product);
-            productService.addProduct(product);
-
+            this.productService.createProduct(product);
             return "redirect:/?message=PRODUCT_CREATED_SUCCESS";
         } catch (Exception exception) {
             return "redirect:/?message=PRODUCT_CREATION_FAILED&error=" + exception.getMessage();
         }
 
     }
-}
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable() Long id) {
+        try {
+            this.productService.deleteProduct(id);
+            return "redirect:/?message=PRODUCT_DELETED_SUCCESSFULLY";
+
+        } catch (Exception exception){
+            return "redirect:/?message=PRODUCT_DELETE_FAILED&error=" + exception.getMessage();
+
+        }
+    }
